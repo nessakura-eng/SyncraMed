f9bc9a652d969df7e234750c8bf258a0e72ee4c0
Vanessa Gutierrez <122655134+nessakura-eng@users.noreply.github.com>
2026-03-10 09:51:00 -0400

    Delete utils/supabase directory

--- utils/supabase/client.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-import { projectId, publicAnonKey } from './info'
-
-// Create a single Supabase client instance to be shared across the app
-export const supabase = createClient(
-  `https://${projectId}.supabase.co`,
-  publicAnonKey
-)
